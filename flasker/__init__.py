5dcab8fea1dd5b0500ef569df4d52dc38bf27c08
Nimish Sule <nimishsule97@gmail.com>
2018-07-21 18:39:37 +0530

    Added requirements.txt

--- flasker/__init__.py
+++ flasker/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
 
 @app.route('/')
 def hello_world():
