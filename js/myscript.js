a9f1b86537dffc527743232de696686ce8af536b
= <37666527+SS-Shivani@users.noreply.github.com>
2018-07-22 04:15:24 +0530

    Removal of files

--- js/myscript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-$('.thumbnail').click(function(){
-      $('.modal-body').empty();
-  	var title = $(this).parent('a').attr("title");
-  	$('.modal-title').html(title);
-  	$($(this).parents('div').html()).appendTo('.modal-body');
-  	$('#myModal').modal({show:true});
-});
-});
