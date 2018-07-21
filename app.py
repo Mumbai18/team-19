050f9650734bf48ff14f6d3b4e17c4cb2c11c1fe
Nimish Sule <nimishsule97@gmail.com>
2018-07-21 20:54:03 +0530

    Added instructions and routes logins and registration

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flasker import app
+from flasker import app, db
 from flasker.models import User
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User}
 
 if __name__ == '__main__':
     app.run()
--- flasker/routes.py
+++ flasker/routes.py
@@ -1,7 +1,23 @@
 from flask import render_template, flash, redirect, url_for, request
 from flasker import app, db
-
+from flask_login import current_user, login_user, logout_user, login_required
+from flasker.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
